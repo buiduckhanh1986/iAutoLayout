42d7bd071e7583a3b85993c2097903506e5bc7f6
Bui Duc Khanh <buiduckhanh@hos-macbook-pro.local>
2016-11-12 12:46:43 +0700

    Initial Commit

---
+++ iAutoLayout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iAutoLayout
+//
+//  Created by Bui Duc Khanh on 11/12/16.
+//  Copyright © 2016 Bui Duc Khanh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
